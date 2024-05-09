c926cdb76014375c2f49507a5549197a2b7fded8
zaid chashoo <zaidchashoo11@gmail.com>
2024-05-09 20:05:37 +0530

    exception handling added

--- use_case_pandas/dataframe.py
+++ use_case_pandas/dataframe.py
@@ -1,23 +1,38 @@
 import sqlite3
 import pandas as pd
+
 db_file = r'C:\Users\Zaid Chashoo\PycharmProjects\ASSIGNMENT\Data Engineer_ETL Assignment.db'
-conn = sqlite3.connect(db_file)
-cursor = conn.cursor()
-df_sales = pd.read_sql_query("SELECT * FROM  sales", conn)
-df_customers = pd.read_sql_query("SELECT * FROM  customers", conn)
-df_orders = pd.read_sql_query("SELECT * FROM  orders", conn)
-df_items = pd.read_sql_query("SELECT * FROM  items", conn)
 
+try:
+    conn = sqlite3.connect(db_file)
+    cursor = conn.cursor()
 
+    df_sales = pd.read_sql_query("SELECT * FROM  sales", conn)
+    df_customers = pd.read_sql_query("SELECT * FROM  customers", conn)
+    df_orders = pd.read_sql_query("SELECT * FROM  orders", conn)
+    df_items = pd.read_sql_query("SELECT * FROM  items", conn)
 
-df_customers = df_customers[df_customers['age'].isin(list(range(18,36)))]
-df_merged = pd.merge(df_orders, df_sales, on='sales_id', how='inner')
-df_merged = pd.merge(df_merged, df_items, on='item_id', how='inner')
-df_merged = pd.merge(df_merged, df_customers, on='customer_id', how='inner')
-df_grouped = df_merged.groupby(['customer_id', 'item_name','age']).agg({'quantity': 'sum'}).reset_index()
-df_grouped = df_grouped[df_grouped['quantity'] > 0]
-df_grouped['quantity'] = df_grouped['quantity'].astype(int)
-df_final = df_grouped[['customer_id', 'age', 'item_name', 'quantity']]
+    df_customers = df_customers[df_customers['age'].isin(list(range(18,36)))]
+    df_merged = pd.merge(df_orders, df_sales, on='sales_id', how='inner')
+    df_merged = pd.merge(df_merged, df_items, on='item_id', how='inner')
+    df_merged = pd.merge(df_merged, df_customers, on='customer_id', how='inner')
+    df_grouped = df_merged.groupby(['customer_id', 'item_name','age']).agg({'quantity': 'sum'}).reset_index()
+    df_grouped = df_grouped[df_grouped['quantity'] > 0]
+    df_grouped['quantity'] = df_grouped['quantity'].astype(int)
+    df_final = df_grouped[['customer_id', 'age', 'item_name', 'quantity']]
 
-print(df_final)
-df_final.to_csv('pd_final.csv', index=False)
+    print(df_final)
+    df_final.to_csv('pd_final.csv', index=False)
+
+except sqlite3.Error as e:
+    print("SQLite error:", e)
+
+except IOError as e:
+    print("I/O error:", e)
+
+except Exception as e:
+    print("Error occurred:", e)
+
+finally:
+    if 'conn' in locals():
+        conn.close()
--- use_case_sql/fetch.py
+++ use_case_sql/fetch.py
@@ -2,38 +2,56 @@ import sqlite3
 import csv
 
 db_file = r'C:\Users\Zaid Chashoo\PycharmProjects\ASSIGNMENT\Data Engineer_ETL Assignment.db'
-conn = sqlite3.connect(db_file)
-cursor = conn.cursor()
-cursor.execute("""
-    SELECT
-        c.customer_id,
-        c.age,
-        i.item_name,
-        SUM(o.quantity) AS quantity
-    FROM
-        orders o
-    JOIN
-        sales s ON o.sales_id = s.sales_id
-    JOIN
-        items i ON o.item_id = i.item_id
-    JOIN
-        customers c ON s.customer_id = c.customer_id
-    WHERE
-        c.age BETWEEN 18 AND 35
-    GROUP BY
-        c.customer_id,
-        c.age,
-        i.item_name
-    HAVING
-        SUM(o.quantity) > 0
-""")
 csv_file_path = 'sql_final.csv'
-rows = cursor.fetchall()
 
+try:
+    conn = sqlite3.connect(db_file)
+    cursor = conn.cursor()
 
-with open(csv_file_path, 'w', newline='') as csvfile:
-    csv_writer = csv.writer(csvfile)
-    csv_writer.writerow(['customer_id', 'age', 'item_name', 'quantity'])
-    csv_writer.writerows(rows)
+    sql_query = """
+        SELECT
+            c.customer_id,
+            c.age,
+            i.item_name,
+            SUM(o.quantity) AS quantity
+        FROM
+            orders o
+        JOIN
+            sales s ON o.sales_id = s.sales_id
+        JOIN
+            items i ON o.item_id = i.item_id
+        JOIN
+            customers c ON s.customer_id = c.customer_id
+        WHERE
+            c.age BETWEEN 18 AND 35
+        GROUP BY
+            c.customer_id,
+            c.age,
+            i.item_name
+        HAVING
+            SUM(o.quantity) > 0
+    """
 
-conn.close()
+    cursor.execute(sql_query)
+    rows = cursor.fetchall()
+    print (rows)
+
+except sqlite3.Error as e:
+    print("SQLite error:", e)
+
+except IOError as e:
+    print("I/O error:", e)
+
+except Exception as e:
+    print("Error occurred:", e)
+
+finally:
+    rows = []  # Initialize rows to an empty list
+    with open(csv_file_path, 'w', newline='') as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(['customer_id', 'age', 'item_name', 'quantity'])
+        csv_writer.writerows(rows)
+
+    if 'conn' in locals():
+        conn.close()
+        print("CSV file created successfully.")
